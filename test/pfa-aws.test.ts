26b060711e68ead5a811f0e004f6e755120c67ff
wadhah <wadhah.mahroug15@gmail.com>
2022-04-14 11:31:30 +0100

    Initial commit

---
+++ test/pfa-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PfaAws from '../lib/pfa-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pfa-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PfaAws.PfaAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
