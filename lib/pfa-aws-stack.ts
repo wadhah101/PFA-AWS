aa37ef99735f4c7a02118d2db0ccacf166893b18
wadhah <wadhah.mahroug15@gmail.com>
2022-04-14 11:35:04 +0100

    init

--- lib/pfa-aws-stack.ts
+++ lib/pfa-aws-stack.ts
@@ -1,16 +1,12 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+interface Props extends StackProps {
+  suffix: string;
+}
 
 export class PfaAwsStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, private props: Props) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PfaAwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
